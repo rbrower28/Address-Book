745edf118c212b1fee7017299508e40849eb899c
Ryan <ryan.brower28@gmail.com>
2023-02-04 14:50:45 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+const mongodb = require('./db/connect');
+const bodyParser = require('body-parser');
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
