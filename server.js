e78b0acd66df0d4e1daa1858269eea4f0188470d
Ryan <ryan.brower28@gmail.com>
2023-02-17 11:57:32 -0700

    oauth login with github

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const port = process.env.port || 3000;
 const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app
   .use(bodyParser.json())
@@ -12,6 +16,31 @@ app
   })
 
 app.use('/', require('./routes'));
+
+app.get('/auth', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
+  );
+});
+
+app.get('/oauth-callback', ({ query: { code } }, res) => {
+  const body = {
+    client_id: process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_SECRET,
+    code,
+  };
+  const opts = { headers: { accept: 'application/json' } };
+  axios
+    .post('https://github.com/login/oauth/access_token', body, opts)
+    .then((_res) => _res.data.access_token)
+    .then((token) => {
+      // eslint-disable-next-line no-console
+      console.log('My token:', token);
+
+      res.redirect(`/?token=${token}`);
+    })
+    .catch((err) => res.status(500).json({ err: err.message }));
+});
 
 process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
